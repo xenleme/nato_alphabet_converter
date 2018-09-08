c0896a3fcaaecfe59b7bd8abc37bb8111a76bdfc
xenleme <xenleme@gmail.com>
2018-09-08 19:20:40 +0300

    Add tests

---
+++ spec/nato_alphabet_converter_spec.rb
@@ -0,0 +1,21 @@
+require './lib/nato_alphabet_converter.rb'
+require 'rspec'
+
+RSpec.describe NatoAlphabetConverter do
+  it 'convert string to nato alphabet' do
+    expect(NatoAlphabetConverter.convert_to_nato('jim')).to eq([
+      'J as Juliet', 'I as India', 'M as Mike'
+    ])
+  end
+
+  it 'convert string to nato alphabet with two or more words' do
+    expect(NatoAlphabetConverter.convert_to_nato('john doe')).to eq([
+      'J as Juliet', 'O as Oscar', 'H as Hotel', 'N as November', ' ',
+      'D as Delta', 'O as Oscar', 'E as Echo'
+    ])
+  end
+
+  it 'returns empty array if passed empty string' do
+    expect(NatoAlphabetConverter.convert_to_nato(' ')).to eq([' '])
+  end
+end
